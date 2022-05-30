a18ae83fe6f6102886488bf737e075d81a220c99
dyfan.jones <dyfan.jones@shopdirect.com>
2022-05-30 13:58:00 +0100

    architecture: move jumpstart to sagemaker.common

--- R/jumpstart_parameters.R
+++
@@ -1,7 +0,0 @@
-# NOTE: This code has been modified from AWS Sagemaker Python:
-# https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/jumpstart/parameters.py
-
-JUMPSTART_DEFAULT_MAX_S3_CACHE_ITEMS = 20
-JUMPSTART_DEFAULT_MAX_SEMANTIC_VERSION_CACHE_ITEMS = 20
-JUMPSTART_DEFAULT_S3_CACHE_EXPIRATION_HORIZON = 6 * 3600
-JUMPSTART_DEFAULT_SEMANTIC_VERSION_CACHE_EXPIRATION_HORIZON = 6 * 3600
